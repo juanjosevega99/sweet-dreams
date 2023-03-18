0d6748d1bf0cd82c9cce4da00199da21755c604c
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2023-03-18 12:07:39 -0500

    fix: import component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 
-import { WakeUp } from "./components/WakeUp";
+import { WakeUp } from "./components/Wakeup/WakeUp";
 
 const hours = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 const minutes = [
