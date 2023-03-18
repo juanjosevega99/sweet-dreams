4f2029d9a5b84805e73fb2535f3494b09c2c0517
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2023-03-18 16:56:55 -0500

    feat: add go back to wakeup component

--- src/components/Wakeup/WakeUp.tsx
+++ src/components/Wakeup/WakeUp.tsx
@@ -2,9 +2,14 @@ interface WakeUpProps {
   selectedHour: number
   selectedMinute: number
   selectedAmPm: string
+  goBack: any
 }
 
-export function WakeUp({ selectedHour, selectedMinute, selectedAmPm }: WakeUpProps) {
+export function WakeUp({ selectedHour, selectedMinute, selectedAmPm, goBack }: WakeUpProps) {
+  const handleGoBackClick = () => {
+    goBack();
+  };
+
   let wakeUpHour = selectedHour % 12;
   if (selectedAmPm === "PM") {
     wakeUpHour += 12;
@@ -16,6 +21,12 @@ export function WakeUp({ selectedHour, selectedMinute, selectedAmPm }: WakeUpPro
         {selectedHour} , {selectedMinute}, {selectedAmPm}
         {/* {wakeUpHour12}:{wakeUpMinute.toString().padStart(2, "0")} {wakeUpAmPm} */}
       </p>
+      <input
+            type="button"
+            value="Go back"
+            className="button-primary"
+            onClick={handleGoBackClick}
+          />
     </div>
   );
 }
