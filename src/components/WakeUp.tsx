6eea179d64d7822b86d22d21486e3afadb9df65a
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2023-03-19 00:11:29 -0500

    feat: show the time for got to sleep

--- src/components/WakeUp.tsx
+++ src/components/WakeUp.tsx
@@ -1,11 +1,16 @@
 interface WakeUpProps {
-  selectedHour: number
-  selectedMinute: number
-  selectedAmPm: string
-  goBack: any
+  selectedHour: number;
+  selectedMinute: number;
+  selectedAmPm: string;
+  goBack: any;
 }
 
-export function WakeUp({ selectedHour, selectedMinute, selectedAmPm, goBack }: WakeUpProps) {
+export function WakeUp({
+  selectedHour,
+  selectedMinute,
+  selectedAmPm,
+  goBack,
+}: WakeUpProps) {
   const handleGoBackClick = () => {
     goBack();
   };
@@ -15,18 +20,48 @@ export function WakeUp({ selectedHour, selectedMinute, selectedAmPm, goBack }: W
     wakeUpHour += 12;
   }
 
+  function findTimeInCycles(cyclesToSleep: number) {
+    const sleepCycleMinutes = 90; // 1.5 hours per sleep cycle
+    const totalSleepMinutes = sleepCycleMinutes * cyclesToSleep;
+    const timeToFallAsleepMinutes = 15; // 15 minutes to fall asleep
+    const totalMinutesNeeded = totalSleepMinutes + timeToFallAsleepMinutes;
+
+    const wakeUpTime = new Date();
+    wakeUpTime.setHours(wakeUpHour);
+    wakeUpTime.setMinutes(selectedMinute);
+
+    const sleepTime = new Date(
+      wakeUpTime.getTime() - totalMinutesNeeded * 60000
+    ); // 60000 ms per minute
+
+    const sleepHour = sleepTime.getHours() % 12;
+    const sleepMinute = sleepTime.getMinutes();
+    const sleepAmPm = sleepTime.getHours() >= 12 ? "PM" : "AM";
+
+    return `${sleepHour}:${sleepMinute} ${sleepAmPm}`;
+  }
+
+  const sixCycles = findTimeInCycles(6);
+  const fiveCycles = findTimeInCycles(5);
+
   return (
-    <div>
-      <p>
-        {selectedHour} , {selectedMinute}, {selectedAmPm}
-        {/* {wakeUpHour12}:{wakeUpMinute.toString().padStart(2, "0")} {wakeUpAmPm} */}
-      </p>
+    <div className="container">
+      <h3>
+        I've to wake up at {selectedHour}:{selectedMinute} {selectedAmPm}
+      </h3>
+      <p>Go to bed at one of the following times:</p>
+
+      <div className="box-times">
+        <div className="time">{sixCycles} for 6 cycles</div>
+        <div className="time">{fiveCycles} for 5 cycles</div>
+      </div>
+
       <input
-            type="button"
-            value="Go back"
-            className="button-primary"
-            onClick={handleGoBackClick}
-          />
+        type="button"
+        value="Go back"
+        className="button-primary"
+        onClick={handleGoBackClick}
+      />
     </div>
   );
 }
