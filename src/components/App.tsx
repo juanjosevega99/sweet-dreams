cf82ee55f07a56293ea97d787fb9ff94f55d6c15
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2023-03-19 08:33:14 -0500

    refactor: use a new array to show and save the value of minutes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,18 +4,18 @@ import { SleepNow } from "./SleepNow";
 
 const hours = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 const minutes = [
-  "00",
-  "05",
-  "10",
-  "15",
-  "20",
-  "25",
-  "30",
-  "35",
-  "40",
-  "45",
-  "50",
-  "55",
+  { value: 0, label: "00" },
+  { value: 5, label: "05" },
+  { value: 10, label: "10" },
+  { value: 15, label: "15" },
+  { value: 20, label: "20" },
+  { value: 25, label: "25" },
+  { value: 30, label: "30" },
+  { value: 35, label: "35" },
+  { value: 40, label: "40" },
+  { value: 45, label: "45" },
+  { value: 50, label: "50" },
+  { value: 55, label: "55" },
 ];
 
 function App() {
@@ -75,8 +75,8 @@ function App() {
             </select>
             <select value={selectedMinute} onChange={handleMinuteChange}>
               {minutes.map((minute) => (
-                <option key={minute} value={minute}>
-                  {minute}
+                <option key={minute.value} value={minute.value}>
+                  {minute.label}
                 </option>
               ))}
             </select>
