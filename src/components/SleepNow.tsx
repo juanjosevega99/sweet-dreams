4150092dc981c462e1d48e07f135668cf8d76b09
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2023-03-19 00:17:54 -0500

    feat: cycles when im going to sleep now

--- src/components/SleepNow.tsx
+++ src/components/SleepNow.tsx
@@ -3,9 +3,38 @@ export function SleepNow({ goBack }: any) {
     goBack();
   };
 
+  // TODO: improve this function
+  function findCyclesToSleep(cyclesToSleep: number) {
+    const sleepCycleMinutes = 90; // 1.5 hours per sleep cycle
+    const totalSleepMinutes = sleepCycleMinutes * cyclesToSleep;
+    const timeToFallAsleepMinutes = 15; // 15 minutes to fall asleep
+    const totalMinutesNeeded = totalSleepMinutes + timeToFallAsleepMinutes;
+
+    const wakeUpTime = new Date();
+    const sleepTime = new Date(
+      wakeUpTime.getTime() - totalMinutesNeeded * 60000
+    ); // 60000 ms per minute
+
+    const sleepHour = sleepTime.getHours() % 12;
+    const sleepMinute = sleepTime.getMinutes();
+    const sleepAmPm = sleepTime.getHours() >= 12 ? "PM" : "AM";
+
+    return `${sleepHour}:${sleepMinute} ${sleepAmPm}`;
+  }
+
+  const fiveCycles = findCyclesToSleep(5);
+  const sixCycles = findCyclesToSleep(6);
+
   return (
     <div>
       <h1>sleep now</h1>
+      <p>Sleep now, wake up at:</p>
+
+      <div className="box-times">
+        <div className="time">{fiveCycles} for 5 cycles</div>
+        <div className="time">{sixCycles} for 6 cycles</div>
+      </div>
+
       <input
         type="button"
         value="Go back"
